06dd92089066fe8381ad12aa8adfe340c5b1283e
sukhknagra <sukh.nagra.sn@gmail.com>
2023-06-12 19:14:54 +1000

    projectList edit

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,5 +1,6 @@
 import Bmi from "../assets/bmi.png";
 import Cflipper from "../assets/CF.jpg";
+import Counter from "../assets/counter.png";
 
 
 export const ProjectList = [
@@ -11,6 +12,12 @@ export const ProjectList = [
     {
         name : "Color Flipper",
         image: Cflipper,
+        skills: "JavaScript, HTML, CSS",
+    },
+
+    {
+        name : "Counter",
+        image: Counter,
         skills: "JavaScript, HTML, CSS",
     },
 ];
